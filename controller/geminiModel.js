c8c0d45dcbea6f4e3fe59e56ce317cda5beee425
Vathsal27 <vathsal.tammewar27@gmail.com>
2024-05-30 23:22:06 +0530

    init commit

--- controller/geminiModel.js
+++ controller/geminiModel.js
@@ -16,7 +16,7 @@ module.exports = {
         const genAI = new GoogleGenerativeAI(process.env.API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
         
-        const prompt = req.body.prompt;
+        const prompt = req.body.prompt + "give prognosis and diagnosis for the following case detail";
 
         const result = await model.generateContent(prompt);
         const response = await result.response;
