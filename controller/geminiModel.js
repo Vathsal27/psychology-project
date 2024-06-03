241903e41393305f9ed6d26d90a2ff700b337843
Vathsal27 <vathsal.tammewar27@gmail.com>
2024-06-03 21:08:36 +0530

    multiple prompts added

--- controller/geminiModel.js
+++ controller/geminiModel.js
@@ -1,8 +1,8 @@
 const { GoogleGenerativeAI } = require('@google/generative-ai');
 
 function formatString(text) {
-    const noAsterisk = text.replace(/\*/g, ""); // Remove all asterisks (*)
-    const noHash = noAsterisk.replace(/#/g, ""); // Remove all hashes (#)
+    const noAsterisk = text.replace(/\*/g, "");
+    const noHash = noAsterisk.replace(/#/g, "");
     return noHash.replace(/\\n/g, "\n");
 }
 
@@ -11,15 +11,22 @@ module.exports = {
         const genAI = new GoogleGenerativeAI(process.env.API_KEY);
         const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-        const devPrompt = "\ngive a provisional diagnosis, differential diagnosis & a final diagnosis with a treatment plan for the above psychological case's json";
-        const prompt = JSON.stringify(req.body) + devPrompt;
+        // const devPrompt = "\ngive a provisional diagnosis, differential diagnosis & a final diagnosis with a treatment plan for the above psychological case's json";
+        const devPrompt1 = "\ngive a possible provisional diagnosis for the above psychological case's json";
+        const devPrompt2 = "\nlist out the differential diagnosis for the above psychological case's json";
+        const devPrompt3 = "\ngive a possible diagnosis with a treatment plan for the above psychological case's json";
+        const devPromptArr = [devPrompt1, devPrompt2, devPrompt3];
+        const data = JSON.stringify(req.body);
 
-        const result = await model.generateContent(prompt);
-        const response = await result.response;
-        const text = response.text();
+        const promises = devPromptArr.map(async (element) => {
+            const result = await model.generateContent(data + element);
+            const response = await result.response;
+            const text = response.text();
+            return formatString(text.replace(/\r?\n/g, ""));
+        });
 
-        return res.status(200).render('../public/result.ejs', {data: formatString(text.replace(/\r?\n/g, ""))});
+        const promptRes = await Promise.all(promises);
 
-        // return res.status(200).json(formatString(text.replace(/\r?\n/g, "")));
+        return res.status(200).render('../public/result.ejs', { promptResults: promptRes });
     }
 };
